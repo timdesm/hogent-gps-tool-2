91007f8255d747867c7b0a160c33ec0cc89db189
Tim De Smet <tim.de.smet@hotmail.be>
2020-04-11 16:57:19 +0200

    Add project files.

---
+++ HoGent GPS Project - Tool 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HoGent_GPS_Project___Tool_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
